cae3988a78f9135bb93e07f1de658bdea779750d
Odin-max <meh84741@gmail.com>
2023-11-20 18:44:24 +0200

    Some changes

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -1,6 +1,9 @@
 # mypy: ignore-errors
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view()),
